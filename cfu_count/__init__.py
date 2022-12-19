65922eadc7ab7078ff0845e485413424a8b1c0aa
Kale Kundert <kale@thekunderts.net>
2022-12-19 17:11:29 -0500

    feat: initial implementation

--- cfu_count/__init__.py
+++ cfu_count/__init__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
+"""
+Infer the cell density of a culture from the number of colonies observed in 
+each spot of a serial dilution.
+"""
+
 __version__ = '0.0.0'
 
-def main():
-    print("Hello world!")
+from .poisson import DilutedPoisson, mean_std
+from .culture import Culture
